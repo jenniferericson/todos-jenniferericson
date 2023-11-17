9ff507e33f9dda0375c082ad5ddc6e744774d7cf
jenniferericson <jenniferericson813@gmail.com>
2023-11-17 11:22:44 +0100

    Created the basic structure of my to do list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import "./../scss/style.scss";
+
+const toDoContainer = document.querySelector("main");
+const title = document.createElement("h3");
+const ul = document.createElement("ul");
+
+title.innerHTML = "To dos måndag";
+
+const toDoList = ["Gå ut med hunden", "Äta frukost", "Göra yoga", "Plugga","Handla"];
+
+for(let i = 0; i < toDoList.length; i++) {
+    let toDos = document.createElement("li");
+
+    toDos.innerHTML = toDoList[i];
+
+    ul.appendChild(toDos);
+}
+
+toDoContainer.appendChild(title);
+toDoContainer.appendChild(ul);
