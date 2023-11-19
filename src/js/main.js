7a3aef0f4254ef43e098a8fb2c7c5f9a9cd1c42d
jenniferericson <jenniferericson813@gmail.com>
2023-11-19 21:03:06 +0100

    done with sort button, and started with complete/undo functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,72 @@
 import "./../scss/style.scss";
+import { toDo } from "./models/toDo";
 
 const toDoContainer = document.querySelector("main");
 const title = document.createElement("h3");
 const ul = document.createElement("ul");
+const sortBtn = document.createElement("button");
 
 title.innerHTML = "To dos måndag";
+sortBtn.innerHTML = "A-Ö";
+
+
 
 toDoContainer.className = "container";
 title.className = "container__title";
 ul.className = "container__ul";
+sortBtn.className = "sortBtn";
 
-const toDoList = ["Gå ut med hunden", "Äta frukost", "Göra yoga", "Plugga","Handla"];
+let toDo1 = new toDo("Gå ut med hunden", false);
+let toDo2 = new toDo("Äta frukost", false);
+let toDo3 = new toDo("Göra yoga", false);
+let toDo4 = new toDo("Plugga", false);
+let toDo5 = new toDo("Handla middag", false);
 
-const stringToDoList = JSON.stringify(toDoList);
-localStorage.setItem("toDoList", stringToDoList);
+const toDoList = [toDo1,toDo2,toDo3,toDo4,toDo5];
 
-for(let i = 0; i < toDoList.length; i++) {
-    let toDos = document.createElement("li");
-    toDos.className = "container__ul--toDos";
+for(let i = 0; i < toDoList.length; i++){
 
-    const doneBtn = document.createElement("button");
-    doneBtn.className = "doneBtn";
+    let taskContainer = document.createElement("li");
+    let text = document.createElement("p");
+    taskContainer.className = "container__ul--toDos";
 
-    toDos.innerHTML = toDoList[i];
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "removeBtn";
 
-    ul.appendChild(toDos);
-    toDos.appendChild(doneBtn);
-}
+    removeBtn.addEventListener("click", () => {
+        toDoList[i].done = true;
+
+        text.className = "doneText";
+
+        let stringToDoList = JSON.stringify(toDoList);
+        localStorage.setItem("toDoList", stringToDoList);
+
+       console.log(toDoList[i]);
+       console.log(toDoList);
+    });
+
+    text.innerHTML = toDoList[i].task;
+
+    const sortFunction = () => {
+        toDoList.sort(function(a, b){
+          let x = a.task.toLowerCase();
+          let y = b.task.toLowerCase();
+          if (x < y) {return -1;}
+          if (x > y) {return 1;}
+          return 0;
+        });
+        text.innerHTML = toDoList[i].task;
+    }
+
+    sortBtn.addEventListener("click", () => {
+        sortFunction ();
+        });
+
+    ul.appendChild(taskContainer);
+    taskContainer.appendChild(removeBtn);
+    taskContainer.appendChild(text);
+};   
 
 toDoContainer.appendChild(title);
+toDoContainer.appendChild(sortBtn);
 toDoContainer.appendChild(ul);
---
+++ src/js/models/toDo.js
@@ -0,0 +1,9 @@
+export class toDo {
+    task;
+    done;
+
+    constructor(task,done) {
+        this.task = task;
+        this.done = done;
+    }
+}
